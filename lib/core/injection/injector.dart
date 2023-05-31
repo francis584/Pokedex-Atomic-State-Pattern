a68d939b0434bdbab128a887104ea1dd003b8e26
Francinildo <francis584@gmail.com>
2023-05-31 15:07:51 -0300

    feature: create autoInjector to dependencies injection

---
+++ lib/core/injection/injector.dart
@@ -0,0 +1,13 @@
+import 'package:auto_injector/auto_injector.dart';
+import 'package:uno/uno.dart';
+
+import 'package:pokedex_egsys/core/constants/api_constants.dart';
+import 'package:pokedex_egsys/features/home/infra/repositories/pokemon_repository_impl.dart';
+
+final autoInjector = AutoInjector();
+setupInjector() {
+  autoInjector.add(() => Uno(baseURL: apiURL));
+  autoInjector.add(PokemonRepositoryImpl.new);
+
+  autoInjector.commit();
+}
