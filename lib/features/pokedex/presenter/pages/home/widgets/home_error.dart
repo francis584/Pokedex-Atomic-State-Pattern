fa7cc72a9ec69640f8e2101edffdd0d749d18780
Francinildo <francis584@gmail.com>
2023-06-04 23:57:10 -0300

    feature: create home_error_widget

--- lib/features/pokedex/presenter/pages/home/widgets/home_error.dart
+++ lib/features/pokedex/presenter/pages/home/widgets/home_error.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import 'package:flutter_svg/flutter_svg.dart';
+
 class HomeError extends StatelessWidget {
   final String error;
   const HomeError({
@@ -10,9 +12,26 @@ class HomeError extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(error),
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            'assets/svg/not-found.svg',
+            semanticsLabel: 'server error',
+            width: 110,
+            height: 110,
+            fit: BoxFit.contain,
+            color: Colors.black,
+          ),
+          Wrap(children: [
+            Text(
+              error,
+              style: TextStyle(color: Colors.black, fontSize: 16),
+            ),
+          ]),
+        ],
       ),
     );
   }
