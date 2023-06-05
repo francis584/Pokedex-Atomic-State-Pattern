eaf9b9344b6ea8b363527702c14cbb84976ea3df
Francinildo <francis584@gmail.com>
2023-06-05 01:50:04 -0300

    feature: recreate homePage with infinite Scrolling

--- lib/features/pokedex/presenter/pages/home/home_page.dart
+++ lib/features/pokedex/presenter/pages/home/home_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'package:pokedex_egsys/core/injection/injector.dart';
-import 'package:pokedex_egsys/features/pokedex/domain/entities/pokemon_entity.dart';
-import 'package:pokedex_egsys/features/pokedex/infra/repositories/pokemon_repository_impl.dart';
+import 'package:asp/asp.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:pokedex_egsys/features/pokedex/presenter/atoms/home_atom.dart';
 import 'package:pokedex_egsys/features/pokedex/presenter/pages/home/widgets/home_error.dart';
 import 'package:pokedex_egsys/features/pokedex/presenter/pages/home/widgets/home_loading.dart';
 import 'package:pokedex_egsys/features/pokedex/presenter/pages/home/widgets/pokemon_item_widget.dart';
@@ -15,34 +16,88 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ScrollController scrollController = ScrollController();
+  @override
+  void initState() {
+    super.initState();
+    initialfetch();
+
+    scrollController.addListener(() {
+      if (!homePokemonsListFinished.value &&
+          scrollController.position.pixels >=
+              (scrollController.position.maxScrollExtent * 0.8)) {
+        fetchMore();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  mostrarToast(String texto) => Fluttertoast.showToast(
+        msg: texto,
+        backgroundColor: Colors.grey[300],
+        textColor: Colors.black,
+        fontSize: 18,
+        toastLength: Toast.LENGTH_LONG,
+      );
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<PokemonEntity>>(
-        future: autoInjector<PokemonRepositoryImpl>()
-            .getPokemons(limit: 20, page: 1),
-        builder: (context, snapshot) {
-          if (snapshot.hasError)
-            return HomeError(error: snapshot.error.toString());
+    context.select(() =>
+        [homeLoading, homePokemonsListFinished, homeError, homePokemonsList]);
 
-          if (snapshot.connectionState == ConnectionState.done &&
-              snapshot.hasData) {
-            return Scaffold(
-                body: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 12,
-                  mainAxisSpacing: 12,
-                  children: snapshot.data!
-                      .map((e) => PokemonItemWidget(pokemonEntity: e))
-                      .toList(),
-                ),
-              ),
-            ));
-          }
+    if (homePokemonsListFinished.value) mostrarToast('Sem mais resultados');
 
-          return HomeLoading();
-        });
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          centerTitle: false,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text(
+              'Pokedex',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  )),
+            )
+          ],
+        ),
+        body: SafeArea(
+          child: RefreshIndicator.adaptive(
+            onRefresh: () async => initialfetch(),
+            child: homeError.value.isNotEmpty
+                ? SingleChildScrollView(
+                    child: HomeError(error: homeError.value))
+                : Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: GridView.count(
+                        controller: scrollController,
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 12,
+                        mainAxisSpacing: 12,
+                        children: [
+                          ...homePokemonsList
+                              .map((e) => PokemonItemWidget(pokemonEntity: e))
+                              .toList(),
+                          if (homeLoading.value)
+                            ...List.generate(8, (index) => HomeLoading()),
+                        ]),
+                  ),
+          ),
+        ));
   }
 }
