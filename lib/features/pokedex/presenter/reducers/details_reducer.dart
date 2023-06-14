dd74bcedb9a21ea3e94476c0fdafaa92241ba33c
Francinildo <francis584@gmail.com>
2023-06-14 14:12:46 -0300

    feature: create Detailsreducer

---
+++ lib/features/pokedex/presenter/reducers/details_reducer.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:asp/asp.dart';
+
+import 'package:pokedex_egsys/core/error/failure.dart';
+import 'package:pokedex_egsys/features/pokedex/domain/usecases/GetPokemonDetailsByIdUsecase.dart';
+import 'package:pokedex_egsys/features/pokedex/presenter/atoms/details_atom.dart';
+
+class DetailsReducer extends Reducer {
+  final GetPokemonDetailsByIdUsecase getPokemonDetailsByIdUsecase;
+
+  DetailsReducer(
+    this.getPokemonDetailsByIdUsecase,
+  ) {
+    on(() => [fetchDetailsById], _fetchDetailsById);
+  }
+
+  _fetchDetailsById() async {
+    try {
+      detailsLoading.setValue(true);
+      detailsError.setValue('');
+      final result =
+          await getPokemonDetailsByIdUsecase(fetchDetailsById.value!);
+
+      detailsPokemonDetailsSelected.setValue(result);
+      detailsLoading.setValue(false);
+    } on Failure catch (e) {
+      detailsError.value = e.message!;
+    }
+  }
+}
