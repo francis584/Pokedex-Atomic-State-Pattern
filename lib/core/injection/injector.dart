d5104005a60ddd85abeb90ea3a7227f5118e939f
Francinildo <francis584@gmail.com>
2023-06-14 14:15:02 -0300

    feature: add GetPokemonDetailsByIdUsecase & DetailsReducer in autoInjector

--- lib/core/injection/injector.dart
+++ lib/core/injection/injector.dart
@@ -2,8 +2,10 @@ import 'package:auto_injector/auto_injector.dart';
 import 'package:uno/uno.dart';
 
 import 'package:pokedex_egsys/core/constants/api_constants.dart';
+import 'package:pokedex_egsys/features/pokedex/domain/usecases/GetPokemonDetailsByIdUsecase.dart';
 import 'package:pokedex_egsys/features/pokedex/domain/usecases/get_all_pokemons_usecase.dart';
 import 'package:pokedex_egsys/features/pokedex/infra/repositories/pokemon_repository_impl.dart';
+import 'package:pokedex_egsys/features/pokedex/presenter/reducers/details_reducer.dart';
 import 'package:pokedex_egsys/features/pokedex/presenter/reducers/home_reducer.dart';
 
 final autoInjector = AutoInjector();
@@ -12,7 +14,10 @@ setupInjector() {
   autoInjector.add(PokemonRepositoryImpl.new);
   autoInjector.add(() => GetAllPokemonsUsecase(
       pokemonRepository: autoInjector<PokemonRepositoryImpl>()));
+  autoInjector.add(() => GetPokemonDetailsByIdUsecase(
+      pokemonRepository: autoInjector<PokemonRepositoryImpl>()));
   autoInjector.addSingleton(HomeReducer.new);
+  autoInjector.addSingleton(DetailsReducer.new);
 
   autoInjector.commit();
 }
