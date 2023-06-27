927769752075bf4aa40c3397b2279fa5f0cce7dd
Francinildo <francis584@gmail.com>
2023-06-26 22:46:06 -0300

    feature: create filterPokemons & clearSearch in state manager system

--- lib/features/pokedex/presenter/atoms/home_atom.dart
+++ lib/features/pokedex/presenter/atoms/home_atom.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/widgets.dart';
+
 import 'package:asp/asp.dart';
 
 import 'package:pokedex_egsys/features/pokedex/domain/entities/pokemon_entity.dart';
@@ -10,8 +12,13 @@ final homeError = Atom<String>('');
 final homePageNumber = Atom(1);
 final homePokemonsList = RxList<PokemonEntity>([]);
 final homeTypesMap = RxMap<TypeEntity, bool>();
+final homeSearchtextEditingController =
+    Atom<TextEditingController>(TextEditingController());
+final homeSearchActive = Atom(false);
 
 // Actions
 final initialfetch = Atom.action();
 final fetchMore = Atom.action();
 final fetchTypes = Atom.action();
+final filterPokemons = Atom.action();
+final clearSearch = Atom.action();
--- lib/features/pokedex/presenter/reducers/home_reducer.dart
+++ lib/features/pokedex/presenter/reducers/home_reducer.dart
@@ -2,21 +2,28 @@
 
 import 'package:asp/asp.dart';
 
+import 'package:pokedex_egsys/core/error/failure.dart';
+import 'package:pokedex_egsys/features/pokedex/domain/entities/type_entity.dart';
 import 'package:pokedex_egsys/features/pokedex/domain/usecases/get_all_pokemons_usecase.dart';
 import 'package:pokedex_egsys/features/pokedex/domain/usecases/get_all_types_usecase.dart';
+import 'package:pokedex_egsys/features/pokedex/domain/usecases/get_pokemons_by_name_&_types_usecase.dart';
 import 'package:pokedex_egsys/features/pokedex/presenter/atoms/home_atom.dart';
 
 class HomeReducer extends Reducer {
   final GetAllPokemonsUsecase getAllPokemonsUsecase;
   final GetAllTypesUsecase getAllTypesUsecase;
+  final GetPokemonsByNameAndTypesUsecase getPokemonsByNameAndTypesUsecase;
 
   HomeReducer(
     this.getAllPokemonsUsecase,
     this.getAllTypesUsecase,
+    this.getPokemonsByNameAndTypesUsecase,
   ) {
     on(() => [initialfetch], _initialfetch);
     on(() => [fetchMore], _fetchMore);
     on(() => [fetchTypes], _fetchTypes);
+    on(() => [filterPokemons], _filterPokemons);
+    on(() => [clearSearch], _clearSearch);
   }
 
   _initialfetch() async {
@@ -81,5 +88,41 @@ class HomeReducer extends Reducer {
     }
 
     homeLoading.value = false;
+  }
+
+  _filterPokemons() async {
+    homeLoading.value = true;
+    homeSearchActive.value = true;
+    homePokemonsList.clear();
+
+    try {
+      final List<TypeEntity> typesSelectedList = [];
+      homeTypesMap.value.forEach((key, value) {
+        if (value) typesSelectedList.add(key);
+      });
+
+      final result = await getPokemonsByNameAndTypesUsecase(
+        GetPokemonsByNameAndTypesParams(
+          name: homeSearchtextEditingController.value.text,
+          types: typesSelectedList,
+        ),
+      );
+
+      homePokemonsList.addAll(result);
+      homePokemonsListFinished.value = true;
+    } on Failure catch (e) {
+      homeError.value = e.message!;
+    } catch (e) {
+      homeError.value = e.toString();
+    }
+
+    homeLoading.value = false;
+  }
+
+  void _clearSearch() {
+    homeSearchtextEditingController.value.clear();
+    homeTypesMap.values.forEach((element) => element = false);
+    homeSearchActive.value = false;
+    _initialfetch();
   }
 }
