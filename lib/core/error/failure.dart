17fadd31362d25a529122cc382fff1f3fad0afc4
Francinildo <francis584@gmail.com>
2023-06-27 03:13:53 -0300

    refactor: remove useless package

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,12 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:equatable/equatable.dart';
 
-class Failure extends Equatable implements Exception {
+class Failure implements Exception {
   final String? message;
   Failure({
     this.message,
   });
-
-  @override
-  List<dynamic> get props => [message];
 }
